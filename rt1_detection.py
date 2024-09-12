ca64fe80699c9290931384bde19cdf445dc15f9c
Hariharan <50658664+Hari1327@users.noreply.github.com>
2024-09-12 13:10:24 +0530

    Update rt1_detection.py

--- rt1_detection.py
+++ rt1_detection.py
@@ -96,7 +96,7 @@ def app():
         """
         
         # Render the HTML video capture
-        st.components.v1.html(video_html)
+        st.components.v1.html(video_html, height=480)
         # Placeholder for the image
         frame_placeholder = st.empty()
         detection_placeholder = st.empty()
