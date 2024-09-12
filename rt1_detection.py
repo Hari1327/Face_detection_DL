96320b1f4a02231c3d78c6d43553bc39cde9cce6
Hariharan <50658664+Hari1327@users.noreply.github.com>
2024-09-12 13:11:56 +0530

    Update rt1_detection.py

--- rt1_detection.py
+++ rt1_detection.py
@@ -96,8 +96,8 @@ def app():
         """
         
         # Render the HTML video capture
-        st.components.v1.html(video_html, height=480)
-        # Placeholder for the image
+        st.components.v1.html(video_html, height=400)
+        # Placeholders for image and detections
         frame_placeholder = st.empty()
         detection_placeholder = st.empty()
 
@@ -112,7 +112,7 @@ def app():
             try:
                 results = model(frame_img)
                 detections = results.pandas().xyxy[0]
-                print(detections)
+                
                 if detections.empty:
                     detection_placeholder.write("No faces detected")
                 else:
